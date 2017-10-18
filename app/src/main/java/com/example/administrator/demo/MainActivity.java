7f2ec6a5cc4a6d13da32d69c0500dc6b41865e84
zZxlk <32754278+zZxlk@users.noreply.github.com>
2017-10-18 20:06:13 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/administrator/demo/MainActivity.java
+++ app/src/main/java/com/example/administrator/demo/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+           //todo
         setContentView(R.layout.activity_main);
+
     }
 }
