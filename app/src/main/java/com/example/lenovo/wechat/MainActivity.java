50d190f0a534447536895ab4b04a1f98d8319876
changao1 <1176512395@qq.com>
2017-11-28 22:40:42 +0800

    kkk

--- app/src/main/java/com/example/lenovo/wechat/MainActivity.java
+++ app/src/main/java/com/example/lenovo/wechat/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.PopupMenu;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -15,13 +16,47 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener, ViewPager.OnPageChangeListener {
     private Toolbar toolbar;
+    // 底部菜单4个Linearlayout
+    private LinearLayout ll_home;
+    private LinearLayout ll_address;
+    private LinearLayout ll_friend;
+    private LinearLayout ll_setting;
+
+    // 底部菜单4个ImageView
+    private ImageView iv_home;
+    private ImageView iv_address;
+    private ImageView iv_friend;
+    private ImageView iv_setting;
+
+    // 底部菜单4个菜单标题
+    private TextView tv_home;
+    private TextView tv_address;
+    private TextView tv_friend;
+    private TextView tv_setting;
+
+    // 中间内容区域
+    private ViewPager viewPager;
+
+    // ViewPager适配器ContentAdapter
+    private ContentAdapter adapter;
+
+    private List<View> views;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 初始化控件
+        initView();
+        // 初始化底部按钮事件
+        initEvent();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("微信");
 
@@ -39,6 +74,55 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+    private void initEvent() {
+        // 设置按钮监听
+        ll_home.setOnClickListener(this);
+        ll_address.setOnClickListener(this);
+        ll_friend.setOnClickListener(this);
+        ll_setting.setOnClickListener(this);
+
+        //设置ViewPager滑动监听
+        viewPager.setOnPageChangeListener(this);
+    }
+    private void initView() {
+
+        // 底部菜单4个Linearlayout
+        this.ll_home = (LinearLayout) findViewById(R.id.ll_home);
+        this.ll_address = (LinearLayout) findViewById(R.id.ll_address);
+        this.ll_friend = (LinearLayout) findViewById(R.id.ll_friend);
+        this.ll_setting = (LinearLayout) findViewById(R.id.ll_setting);
+
+        // 底部菜单4个ImageView
+        this.iv_home = (ImageView) findViewById(R.id.iv_home);
+        this.iv_address = (ImageView) findViewById(R.id.iv_address);
+        this.iv_friend = (ImageView) findViewById(R.id.iv_friend);
+        this.iv_setting = (ImageView) findViewById(R.id.iv_setting);
+
+        // 底部菜单4个菜单标题
+        this.tv_home = (TextView) findViewById(R.id.tv_home);
+        this.tv_address = (TextView) findViewById(R.id.tv_address);
+        this.tv_friend = (TextView) findViewById(R.id.tv_friend);
+        this.tv_setting = (TextView) findViewById(R.id.tv_setting);
+
+        // 中间内容区域ViewPager
+        this.viewPager = (ViewPager) findViewById(R.id.vp_content);
+
+        // 适配器
+        View page_01 = View.inflate(MainActivity.this, R.layout.page_01, null);
+        View page_02 = View.inflate(MainActivity.this, R.layout.page_02, null);
+        View page_03 = View.inflate(MainActivity.this, R.layout.page_03, null);
+        View page_04 = View.inflate(MainActivity.this, R.layout.page_04, null);
+
+        views = new ArrayList<View>();
+        views.add(page_01);
+        views.add(page_02);
+        views.add(page_03);
+        views.add(page_04);
+
+        this.adapter = new ContentAdapter(views);
+        viewPager.setAdapter(adapter);
+
     }
 
     @Override
@@ -61,7 +145,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
+        // automatically handle clicks on the Home/Up ButtonActivity, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
@@ -97,4 +181,87 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    public void onClick(View view) {
+        // 在每次点击后将所有的底部按钮(ImageView,TextView)颜色改为灰色，然后根据点击着色
+        restartBotton();
+        // ImageView和TetxView置为绿色，页面随之跳转
+        switch (view.getId()) {
+            case R.id.ll_home:
+                iv_home.setImageResource(R.drawable.tab_wechat_selected);
+                tv_home.setTextColor(0xff1B940A);
+                viewPager.setCurrentItem(0);
+                break;
+            case R.id.ll_address:
+                iv_address.setImageResource(R.drawable.tab_contacts_selected);
+                tv_address.setTextColor(0xff1B940A);
+                viewPager.setCurrentItem(1);
+                break;
+            case R.id.ll_friend:
+                iv_friend.setImageResource(R.drawable.tab_friend_selected);
+                tv_friend.setTextColor(0xff1B940A);
+                viewPager.setCurrentItem(2);
+                break;
+            case R.id.ll_setting:
+                iv_setting.setImageResource(R.drawable.tab_settings_selected);
+                tv_setting.setTextColor(0xff1B940A);
+                viewPager.setCurrentItem(3);
+                break;
+
+            default:
+                break;
+        }
+
+    }
+    private void restartBotton() {
+        // ImageView置为灰色
+        iv_home.setImageResource(R.drawable.tab_wechat_normal);
+        iv_address.setImageResource(R.drawable.tab_contacts_normal);
+        iv_friend.setImageResource(R.drawable.tab_friend_normal);
+        iv_setting.setImageResource(R.drawable.tab_settings_normal);
+        // TextView置为白色
+        tv_home.setTextColor(0xffffffff);
+        tv_address.setTextColor(0xffffffff);
+        tv_friend.setTextColor(0xffffffff);
+        tv_setting.setTextColor(0xffffffff);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int arg0) {
+
+    }
+    @Override
+    public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+    }
+
+    @Override
+    public void onPageSelected(int arg0) {
+        restartBotton();
+        //当前view被选择的时候,改变底部菜单图片，文字颜色
+        switch (arg0) {
+            case 0:
+                iv_home.setImageResource(R.drawable.tab_wechat_selected);
+                tv_home.setTextColor(0xff1B940A);
+                break;
+            case 1:
+                iv_address.setImageResource(R.drawable.tab_contacts_selected);
+                tv_address.setTextColor(0xff1B940A);
+                break;
+            case 2:
+                iv_friend.setImageResource(R.drawable.tab_friend_selected);
+                tv_friend.setTextColor(0xff1B940A);
+                break;
+            case 3:
+                iv_setting.setImageResource(R.drawable.tab_settings_selected);
+                tv_setting.setTextColor(0xff1B940A);
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
 }
